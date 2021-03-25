daf0332274c8fc3154f4270e32dbc49382466645
marianaRodLima <marianarlima93@gmail.com>
2021-03-25 11:19:36 -0300

    Lista de pets atualizada com template string

--- index.js
+++ index.js
@@ -37,8 +37,10 @@ let pets = [{
 ];
 
 const listarPets = () => {
-    for(let i = 0; i < pets.length; i++)
-    console.log(pets[i].nome);
+    for(let pet of pets) {
+   // console.log(pets[i].nome);
+    console.log(`O nome do pet é ${pet.nome}, ${pet.idade}, ${pet.tipo}, ${pet.raça}`);
+}
 }
 
 listarPets();
